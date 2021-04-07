f70452e8911a0a86781245c7cfcd4bd38111c609
Diego Alvarez <dendrit83@protonmail.ch>
2021-04-06 23:31:51 -0300

    tiding up and refactoring

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -4,34 +4,39 @@
 * by Diego Alvarez @doc on slack.
 * April 2021.
 ================================================*/
-//console.log("test"); - Testing html-js connection.
+//console.log("test"); - Testing html<-->js connection.
 
-// setting the focus on the name field of the form when page loads.
+// 1) setting the focus on the name field of the form when page loads.
 const nameField = document.querySelector('#name');
 nameField.focus();
 
-// job role functionality - Other text input hidden by default.
+// 2) "Other" job role functionality -
 const otherJobText = document.querySelector("#other-job-role");
-otherJobText.hidden = true;
-const titleSelected = document.querySelector("#title");
-titleSelected.addEventListener("change", (e) => {
-	e.target.value === "other" ? otherJobText.hidden = false : otherJobText.hidden = true;
-});
+otherJobText.hidden = true; // Other text input hidden by default.
+const roleSelected = document.querySelector('#title');
+roleSelected.addEventListener( 'change', e => {
+	e.target.value === 'other' ?
+  otherJobText.hidden = false : otherJobText.hidden = true;
+}); //using ternary operator for simple branching.
 
-// Color selector functionality.
+// 3) Color selector functionality.
 const colorItems = document.querySelector("#color");
-colorItems.disabled = true;
 const designSelector = document.querySelector("#design");
-designSelector.addEventListener("change", (e) => {
+colorItems.disabled = true;
+designSelector.addEventListener( 'change', e => {
 	colorItems.disabled = false;
-  if (e.target.value === "js puns") {
+  if ( e.target.value === "js puns" ) {
     colorItems.children[1].selected = true;
   } else {
     colorItems.children[4].selected = true;
   }
-	for (let i = 1; i < colorItems.children.length; i++) {
-		colorItems.children[i].textContent = `${colorItems.children[i].textContent.replace(/\(.*\)/, "")}`;
-		if (colorItems.children[i].dataset.theme === e.target.value) {
+  /* for looping through all colors, cleaning with REGEX the textContent,
+  *  and hidding colors that do not correspond to the selected design.
+  */
+  for (let i = 1; i < colorItems.children.length; i++) {
+		colorItems.children[i].textContent =
+    `${colorItems.children[i].textContent.replace(/\(.*\)/, "")}`;
+    if ( colorItems.children[i].dataset.theme === e.target.value ) {
 			colorItems.children[i].hidden = false;
 		} else {
 			colorItems.children[i].hidden = true;
@@ -39,13 +44,13 @@ designSelector.addEventListener("change", (e) => {
 	}
 });
 
-// Register for Activities section
+// 4) Register for Activities functionality:
 const sumActivities = document.querySelector("#activities-cost");
 const activitiesField = document.querySelector("#activities");
-let sumTotal = 0;
-activitiesField.addEventListener("change", (e) => {
-	if (e.target.checked) {
-		sumTotal += parseInt(e.target.dataset.cost);
+let sumTotal = 0; // counter variable.
+activitiesField.addEventListener( 'change', e => {
+	if ( e.target.checked ) {
+		sumTotal += parseInt( e.target.dataset.cost );
 		sumActivities.textContent = `Total: ${sumTotal}`;
 	} else {
 		sumTotal -= parseInt(e.target.dataset.cost);
@@ -53,29 +58,31 @@ activitiesField.addEventListener("change", (e) => {
 	}
 });
 
-// Payment info section
+// 5) Payment info functionality:
 const paymentMethod = document.querySelector("#payment");
 const credit = document.querySelector("#credit-card");
 const paypal = document.querySelector("#paypal");
 const btc = document.querySelector("#bitcoin");
-paymentMethod.children[1].selected = true;
+paymentMethod.children[1].selected = true; // Credit Card default method.
 paypal.hidden = true;
 btc.hidden = true;
-paymentMethod.addEventListener("change", (e) => {
-	if (e.target.value === "credit-card") {
-		credit.hidden = false;
-		paypal.hidden = true;
-		btc.hidden = true;
+const showPayMethod = element => element.hidden = false;
+
+paymentMethod.addEventListener( 'change', e => {
+  const payArr = [credit, paypal, btc];
+  for (let i of payArr) {
+    i.hidden = true;
+  }
+  if (e.target.value === "credit-card") {
+		showPayMethod(credit);
 	} else if (e.target.value === "paypal") {
-		credit.hidden = true;
-		paypal.hidden = false;
-		btc.hidden = true;
+		showPayMethod(paypal);
 	} else if (e.target.value === "bitcoin") {
-		credit.hidden = true;
-		paypal.hidden = true;
-		btc.hidden = false;
+		showPayMethod(btc);
 	}
 });
+
+// 6) form validation. using regex and custom helper functions.
 
 function validateName(name) {
 	if (name === "" || /^\s+$/.test(name)) {
