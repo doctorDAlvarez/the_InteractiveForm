c5a1365a4183f34a0368d71e70b71bd274d1ebfb
Diego Alvarez <dendrit83@protonmail.ch>
2021-04-07 00:54:56 -0300

    Update script.js

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -66,8 +66,9 @@ const btc = document.querySelector("#bitcoin");
 paymentMethod.children[1].selected = true; // Credit Card default method.
 paypal.hidden = true;
 btc.hidden = true;
-const showPayMethod = element => element.hidden = false;
 
+// arrow fx to show the correct payment method selected.
+const showPayMethod = element => element.hidden = false;
 paymentMethod.addEventListener( 'change', e => {
   const payArr = [credit, paypal, btc];
   for (let i of payArr) {
@@ -83,39 +84,39 @@ paymentMethod.addEventListener( 'change', e => {
 });
 
 // 6) form validation. using regex and custom helper functions.
+//======================
+// Validation arrow functions
+//======================
 
-function validateName(name) {
-	if (name === "" || /^\s+$/.test(name)) {
-		return false;
-	} else {
-		return true;
-	}
+const validateName = name => (name === "" || /^\s+$/.test(name));
+
+const validateEmail = email => !(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email));
+
+const validateCardNumber = number => !(/^\d{13,16}$/.test(number));
+
+const validateZip = zip => !(/^\d{5}$/.test(zip));
+
+const validateCVV = cvv => !(/^\d{3}$/.test(cvv));
+
+function errorStatus(element) {
+  element.nextElementSibling.style.display = "inherit";
+  element.parentElement.classList.add("not-valid");
+  element.parentElement.classList.remove("valid");
+}
+function validStatus(element) {
+  element.nextElementSibling.style.display = "none";
+  element.parentElement.classList.add("valid");
+  element.parentElement.classList.remove("not-valid");
 }
 
-function validateEmail(email) {
-	return (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email));
+function validation(func, element, e) {
+  if ( func(element.value) ) {
+    e.preventDefault();
+    errorStatus(element);
+  } else {
+    validStatus(element);
+  }
 }
-
-function validateActivities() {
-	if (sumTotal) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function validateCardNumber(number) {
-  return (/^\d{13,16}$/.test(number));
-}
-function validateZip(zip) {
-  return (/^\d{5}$/.test(zip));
-}
-
-function validateCVV(cvv) {
-  return (/^\d{3}$/.test(cvv));
-}
-
-
 //form validation when submitted.
 const email = document.querySelector("#email");
 const card = document.querySelector("#cc-num");
@@ -123,74 +124,23 @@ const zip = document.querySelector("#zip");
 const cvv = document.querySelector("#cvv");
 const formElement = document.getElementsByTagName("form")[0];
 
-formElement.addEventListener("submit", (e) => {
-	if (!validateName(nameField.value)) {
+formElement.addEventListener("submit", e => {
+  validation(validateName, nameField, e);
+  validation(validateEmail, email, e);
+  if ( !sumTotal ) {
 		e.preventDefault();
-		document.querySelector("#name-hint").style.display = "inherit";
-    nameField.parentElement.classList.add("not-valid");
-    nameField.parentElement.classList.remove("valid");
-	} else {
-		document.querySelector("#name-hint").style.display = "none";
-    nameField.parentElement.classList.add("valid");
-    nameField.parentElement.classList.remove("not-valid");
-	}
-	if (!validateEmail(email.value)) {
-		e.preventDefault();
-		document.querySelector("#email-hint").style.display = "inherit";
-    email.parentElement.classList.add("not-valid");
-    email.parentElement.classList.remove("valid");
-	} else {
-		document.querySelector("#email-hint").style.display = "none";
-    email.parentElement.classList.add("valid");
-    email.parentElement.classList.remove("not-valid");
-	}
-	if (!validateActivities()) {
-		e.preventDefault();
-		document.querySelector("#activities-hint").style.display = "inherit";
+		activitiesField.lastElementChild.style.display = "inherit";
     activitiesField.classList.add("not-valid");
     activitiesField.classList.remove("valid");
 	} else {
-		document.querySelector("#activities-hint").style.display = "none";
+		activitiesField.lastElementChild.style.display = "none";
     activitiesField.classList.add("valid");
     activitiesField.classList.remove("not-valid");
-
   }
-
-
   if (credit.hidden === false) {
-    if (!validateCardNumber(card.value)) {
-      e.preventDefault();
-      card.nextElementSibling.style.display = "inherit";
-      card.parentElement.classList.add("not-valid");
-      card.parentElement.classList.remove("valid");
-    } else {
-      card.nextElementSibling.style.display = "none";
-      card.parentElement.classList.add("valid");
-      card.parentElement.classList.remove("not-valid");
-    }
-
-    if (!validateZip(zip.value)) {
-      e.preventDefault();
-      zip.nextElementSibling.style.display = "inherit";
-      zip.parentElement.classList.add("not-valid");
-      zip.parentElement.classList.remove("valid");
-    } else {
-      zip.nextElementSibling.style.display = "none";
-      zip.parentElement.classList.add("valid");
-      zip.parentElement.classList.remove("not-valid");
-    }
-
-    if (!validateCVV(cvv.value)) {
-      e.preventDefault();
-      cvv.nextElementSibling.style.display = "inherit";
-      cvv.parentElement.classList.add("not-valid");
-      cvv.parentElement.classList.remove("valid");
-    } else {
-      cvv.nextElementSibling.style.display = "none";
-      cvv.parentElement.classList.add("valid");
-      cvv.parentElement.classList.remove("not-valid");
-    }
-
+    validation(validateCardNumber, card, e);
+    validation(validateZip, zip, e);
+    validation(validateCVV, cvv, e);
   }
 });
 
